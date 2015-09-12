4066d941bfabc12e5166101b7d7914a694cf85c3
Orivej Desh <orivej@gmx.fr>
2015-09-12 12:42:29 +0000

    Use miekg/dns.

--- main.go
+++ main.go
@@ -3,100 +3,64 @@ package main
 import (
 	"flag"
 	"log"
-	"net"
-	"time"
+	"sync"
+
+	"github.com/miekg/dns"
 )
 
 var (
-	flUpstreamIP = IPValueFlag("upstream-ip", net.IP{127, 0, 0, 1},
-		"IP address of upstream DNS server")
-	flUpstreamPort = flag.Int("upstream-port", 53,
-		"UDP port of upstream DNS server")
+	flUpstream = flag.String("upstream", "127.0.0.1:53",
+		"Upstream DNS server")
+)
+
+var (
+	cache     = map[string]*dns.Msg{}
+	cacheLock = sync.Mutex{}
+	dnsclient = dns.Client{SingleInflight: true}
 )
 
 func main() {
 	flag.Parse()
-	serve()
+
+	server := dns.Server{Net: "udp"}
+	dns.HandleFunc(".", serve)
+	err := server.ListenAndServe()
+	eexit(err)
 }
 
-func serve() {
-	upstreamUDP := net.UDPAddr{IP: flUpstreamIP.ip, Port: *flUpstreamPort}
-	localUDP := net.UDPAddr{Port: 53}
-
-	in, err := net.ListenUDP("udp", &localUDP)
-	eexit(err)
-	defer eclose(in)
-
-	out, err := net.DialUDP("udp", nil, &upstreamUDP)
-	eexit(err)
-	defer eclose(out)
-
-	cache := map[string][]byte{}
-	inbuf := make([]byte, 65536)
-	inid := make([]byte, 2)
-	outbuf := make([]byte, 65536)
-	counter := uint16(0)
-
-mainLoop:
-	for {
-		// read incoming
-		n, inaddr, err := in.ReadFromUDP(inbuf)
-		if eprint(err) || n < 2 {
-			continue
-		}
-		copy(inid, inbuf) // Transaction ID
-
-		// prepare response
-		key := string(inbuf[2:])
-		var value []byte
-		var ok bool
-		if value, ok = cache[key]; !ok {
-			log.Println("mainLoop (no cache)")
-			// prepare for request to timout
-		requestLoop:
-			for {
-				log.Println("requestLoop")
-				// query upstream
-				counter++
-				inbuf[0] = byte(counter >> 8)
-				inbuf[1] = byte(counter & 0xff)
-				_, err = out.Write(inbuf[:n])
-				if eprint(err) {
-					continue mainLoop
-				}
-
-				// read upstream, dealing with outdated responses
-				for {
-					log.Println("responseLoop")
-					err := out.SetReadDeadline(time.Now().Add(500 * time.Millisecond))
-					if eprint(err) {
-						continue mainLoop
-					}
-
-					n, err = out.Read(outbuf)
-					if neterr, ok := err.(net.Error); ok && neterr.Timeout() {
-						continue requestLoop
-					} else if eprint(err) {
-						continue mainLoop
-					}
-					if inbuf[0] == outbuf[0] && inbuf[1] == outbuf[1] {
-						// Transaction IDs match
-						break requestLoop
-					}
-				}
-			}
-
-			// save in cache
-			value = make([]byte, n)
-			copy(value, outbuf)
-			cache[key] = value
-		}
-
-		// answer incoming
-		copy(value, inid) // Transaction ID
-		_, err = in.WriteToUDP(value, inaddr)
-		if eprint(err) {
-			continue
-		}
+func msgKey(m *dns.Msg) string {
+	t := ""
+	if t1, ok := dns.TypeToString[m.Question[0].Qtype]; ok {
+		t = t1
 	}
+	cl := ""
+	if cl1, ok := dns.ClassToString[m.Question[0].Qclass]; ok {
+		cl = cl1
+	}
+	return m.Question[0].Name + " " + t + " " + cl
+}
+
+func serve(w dns.ResponseWriter, req *dns.Msg) {
+	var resp *dns.Msg
+	var err error
+	key := msgKey(req)
+	cacheLock.Lock()
+	resp, ok := cache[key]
+	cacheLock.Unlock()
+	if ok {
+		// SingleInflight ensures that resp will not be changed until the end of serve
+		resp.Id = req.Id
+	} else {
+		resp, _, err = dnsclient.Exchange(req, *flUpstream)
+		if err != nil {
+			log.Printf("Failed to handle %v: %v\n", key, err)
+			dns.HandleFailed(w, req)
+			return
+		}
+		cacheLock.Lock()
+		cache[key] = resp
+		cacheLock.Unlock()
+	}
+	err = w.WriteMsg(resp)
+	eprint(err)
 }
